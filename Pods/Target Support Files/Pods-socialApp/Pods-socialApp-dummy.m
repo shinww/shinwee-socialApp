88eb6bab2c9dfafa16d8799ea7de2068dc4fd370
shinww <shinw.bernardyap@gmail.com>
2017-01-21 16:14:03 +0800

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-socialApp/Pods-socialApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_socialApp : NSObject
+@end
+@implementation PodsDummy_Pods_socialApp
+@end
---
+++ socialApp/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialApp
+//
+//  Created by shinwee on 1/21/17.
+//  Copyright © 2017 shinwee. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
